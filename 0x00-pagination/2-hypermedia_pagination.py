bb88f13b9aa33d74ba189f8ba5b010d560425a31
Amili-usain <amilihussein073@gmail.com>
2023-05-12 19:02:17 +0000

    Hypermedia pagination

---
+++ 0x00-pagination/2-hypermedia_pagination.py
@@ -0,0 +1,82 @@
+#!/usr/bin/env python3
+"""This module provides a Server class that can be used to paginate a database
+of popular baby names. The Server class contains a dataset of baby names
+and provides methods to retrieve pages of data and information about the pages.
+"""
+import csv
+import math
+from typing import Dict, List, Tuple
+
+
+def index_range(page: int, page_size: int) -> Tuple[int, int]:
+    """Given a page number and page size return the start and end indices of
+the page."""
+    start = (page - 1) * page_size
+    end = start + page_size
+    return (start, end)
+
+
+class Server:
+    """Server class to paginate a database of popular baby names."""
+
+    # The path to the CSV data file
+    DATA_FILE = "Popular_Baby_Names.csv"
+
+    def __init__(self):
+        """Initializes a new Server instance."""
+        self.__dataset = None
+
+    def dataset(self) -> List[List]:
+        """Returns the dataset, reading it from the file if necessary.
+
+        Returns:
+            List[List]: The dataset.
+        """
+        if self.__dataset is None:
+            with open(self.DATA_FILE) as f:
+                reader = csv.reader(f)
+                dataset = [row for row in reader]
+            self.__dataset = dataset[1:]
+
+        return self.__dataset
+
+    def get_page(self, page: int = 1, page_size: int = 10) -> List[List]:
+        """Retrieves a page of data.
+
+        Args:
+            page (int, optional): The page number. Defaults to 1.
+            page_size (int, optional): The page size. Defaults to 10.
+
+        Returns:
+            List[List]: The requested page of data.
+        """
+        assert type(page) == int and type(page_size) == int
+        assert page > 0 and page_size > 0
+        start, end = index_range(page, page_size)
+        data = self.dataset()
+        if start > len(data):
+            return []
+        return data[start:end]
+
+    def get_hyper(self, page: int = 1, page_size: int = 10) -> Dict:
+        """Retrieves information about a page.
+
+        Args:
+            page (int, optional): The page number. Defaults to 1.
+            page_size (int, optional): The page size. Defaults to 10.
+
+        Returns:
+            Dict: A dictionary containing information about the requested page.
+        """
+        page_data = self.get_page(page, page_size)
+        start, end = index_range(page, page_size)
+        total_pages = math.ceil(len(self.__dataset) / page_size)
+        page_info = {
+            'page_size': len(page_data),
+            'page': page,
+            'data': page_data,
+            'next_page': page + 1 if end < len(self.__dataset) else None,
+            'prev_page': page - 1 if start > 0 else None,
+            'total_pages': total_pages,
+        }
+        return page_info
